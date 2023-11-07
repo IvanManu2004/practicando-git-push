4734b46e9bf08154cea71c06994ce1a822d8a519
IvanManu2004 <Ivanmanu2004@gmail.com>
2023-11-07 11:00:13 +0100

    añadidos mis datos

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Mi nombre es Iván Manuel Fernández Fernández");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("Mi instituto es el IES San Andres");
+    System.out.println("Vivo en Villabalter");
   }
   
 }
